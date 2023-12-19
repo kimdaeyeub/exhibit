71bcaecae503fb0cc350951a5fe6acdec1856875
kimdaeyeub <kdy3915@naver.com>
2023-12-20 03:14:48 +0900

    update responsive home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen text-6xl font-semibold bg-yellow-100 px-52 dark:bg-gray-900"></div>
+    <div className="w-full h-screen text-6xl font-semibold bg-yellow-100 xl:px-52 md:px-32 px-12 dark:bg-gray-900"></div>
   );
 }

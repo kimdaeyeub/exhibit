8660dfe653928e162c1ca4fe85b639be4f04b6c8
kimdaeyeub <kdy3915@naver.com>
2023-12-20 16:47:10 +0900

    브렌치 테스트 진짜 끝

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
 
 export default function Home() {
-  //브렌치 테스트중입니다.
-  //마스터 브렌치 테스트
-  //브렌치 테스트 끝
   return (
     <div className="w-full h-screen text-6xl font-semibold bg-yellow-100 xl:px-52 lg:px-44 md:px-32 px-12 dark:bg-gray-900"></div>
   );
